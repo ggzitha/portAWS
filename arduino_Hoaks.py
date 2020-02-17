b00461f62e8a7eb4a3407e7d2acc5e0b603687f1
ggzitha <kenconex@gmail.com>
2020-02-18 04:29:09 +0700

    "initial commit"

--- arduino_Hoaks.py
+++ arduino_Hoaks.py
@@ -56,8 +56,8 @@ def printit():
     print(data_arrays)
 
     with cd("~/Desktop/Read/"):
-        subprocess.call(["git", "add", "."])
+        subprocess.call(["git", "add", "."], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         subprocess.call(["git", "commit", "-m", "\"initial commit\""])
-        subprocess.call(["git", "push", "-u", "origin", "master"])
+        subprocess.call(["git", "push", "-u", "--force" , "origin", "master"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 printit()
